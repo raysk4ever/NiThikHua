e61fb4530dfeb0497027aef6affe571211fc5bf6
unknown <raysk7161@gmail.com>
2019-08-02 18:38:11 +0530

    added landing page

---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Landing1 from "../src/components/landing1";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Landing1 />
+        <Landing1 />
+        <Landing1 />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/landing1.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/landing1.css";
+
+const Landing1 = () => {
+  return (
+    <div className="landing1wrapper">
+      <div className="title">
+        <p className="title_header">NiThikHua</p>
+        <div className="search_bar">
+          <p>seach for medicine...</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing1;
